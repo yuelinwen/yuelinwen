5e207cb7b253d7c6b4786af21fc780e630677dfe
Yuelin Wen <wenyuelinca@gmail.com>
2025-04-17 13:18:01 -0400

    config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from "next";
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: "export",
 
-const nextConfig: NextConfig = {
-  /* config options here */
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  distDir: "dist",
+  images:{
+    unoptimized:true
+  }
 };
 
-export default nextConfig;
+module.exports = nextConfig;
