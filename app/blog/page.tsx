d131813e5097bc026c661c1239375de334b396eb
Yuelin Wen <wenyuelinca@gmail.com>
2025-04-20 00:53:59 -0400

    add certificate

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Page = () => {
     return (
         <div>
-            Blog
+            To be continue...
         </div>
     );
 }
--- app/certifications/page.tsx
+++ app/certifications/page.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { prefix } from '@/util/prefix';
+import Image from 'next/image';
+import { AspectRatio } from '@/components/ui/aspect-ratio';
 
 const Certifications = () => {
     return (
-        <div>
-            Certifications
+        <div className="flex flex-col gap-20">
+            <div className="flex flex-col gap-3">
+                <h3 className="text-lg font-semibold">Comptia Network+ </h3>
+                <ul className="gap-1">
+                    <li className='flex flex-row'>
+                        <AspectRatio ratio={16 / 9}>
+                            <Image src={`${prefix}/image/comptia.png`} width={480} height={1} alt="CloudVaut home page" className="rounded-md object-cover" />
+                        </AspectRatio>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 }
