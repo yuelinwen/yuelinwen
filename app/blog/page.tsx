fed2d7200b9c59a2ed87dd94c34b78f811582846
Yuelin Wen <wenyuelinca@gmail.com>
2025-04-20 01:12:07 -0400

    update

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 const Page = () => {
     return (
-        <div>
-            To be continue...
+        <div className="flex flex-col gap-20">
+            <div className="flex flex-col gap-3">
+                <h3 className="text-lg font-semibold">My first blog </h3>
+                <p className="italic">personal blog</p>
+                <p>This is a personal website that I built myself. I plan to use it as a demonstration of my work, and also as a space to share blogs about my experiences and interests. It's a work in progress—more content coming soon!</p>
+            </div>
         </div>
     );
 }
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <div>
-            contract
+        <div className="flex flex-col gap-20">
+            <div className="flex flex-col gap-3">
+                <h3 className="text-lg font-semibold">📧 Email: <a href="mailto:wenyuelinca@gmail.com">wenyuelinca@gmail.com</a> </h3>
+            </div>
         </div>
     );
 }
