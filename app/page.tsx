57ce72e6ff1597850e8e3ea31f97be1ba1884eb9
Yuelin Wen <wenyuelinca@gmail.com>
2025-04-30 01:39:29 -0400

    update

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
         <h1 className="text-2xl font-semibold">Research Interests</h1>
         <div>
           <h3 className="text-lg font-semibold">Artificial intelligence and real world application</h3>
-          <p>I am particularly interested in the application of artificial intelligence techniques in embedded systems for real-world solutions.</p>
+          <p>I am particularly interested in the application of artificial intelligence techniques in microelectronics for real-world solutions.</p>
           <p>During my undergraduate studies, I took elective courses in AI, neural networks, and real-time systems, which strengthened my passion for intelligent system design. I believe that by combining microelectronic systems with AI methods, we can create smart, adaptive technologies that operate efficiently to address real world problems.</p>
           <p>I hope to contribute to the development of practical, intelligent electronic systems that enhance everyday technologies in fields such as robotics, autonomous vehicles, and IoT devices.</p>
         </div>
